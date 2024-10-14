0d96c0ba2d2492410a555995ac0bb2f2c0c31d12
Witaya252525 <witaya.c@kkumail.com>
2024-10-14 13:50:16 +0700

    OCT_14_14

--- Link.py
+++ Link.py
@@ -1,4 +1,33 @@
 
 https://appbrewery.com/p/legacy-complete-web-development-course/
 
+CSS
+
+https://developer.mozilla.org/en-US/docs/Web/CSS/width
+
+
+https://appbrewery.github.io/css-inspection/
+
+
+https://chromewebstore.google.com/detail/pesticide-for-chrome/bakpbgckdnepkmkeaiomhmfcnejndkbi?pli=1
+
+color
+
+https://developer.mozilla.org/en-US/docs/Web/CSS/named-color
+
+
+color-slide
+
+https://www.csfieldguide.org.nz/en/interactives/rgb-mixer/
+
+
+https://www.colorhunt.co/
+
+https://unsplash.com/t/wallpapers
+
+
+
+
+
+
 
